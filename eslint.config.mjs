dfa5525c014c0d70e90c51b9ca6243274d55ad01
John Zhou <zhou9865@gmail.com>
2024-08-01 19:03:41 -0700

    added custom github action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default [
   {
     rules: {
       "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
